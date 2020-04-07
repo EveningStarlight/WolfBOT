4a2042bb7f0911e04a8ec7f3703c322f04058b54
Adam <55841241+AdamPrins@users.noreply.github.com>
2020-04-07 19:35:31 -0400

    Cant nominate while voting
    Made a check for voting
    Added command for third (same as second)

--- bot.js
+++ bot.js
@@ -238,7 +238,7 @@ client.on('message', async msg => {
 			case 'nominate':
 				let accused = guild.members.find(user => user.id === msg.mentions.users.first().id);
 				
-				if (isDay && user.roles.has(discordRoles.Town.id) && accused.roles.has(discordRoles.Town.id) && accused.id != user.id) {
+				if (isDay && vote == null && user.roles.has(discordRoles.Town.id) && accused.roles.has(discordRoles.Town.id) && accused.id != user.id) {
 					if (nominate.nominations[user.displayName.toLowerCase()] != null) {
 						nominate.accused[nominate.nominations[user.displayName.toLowerCase()].nominated.displayName.toLowerCase()] = null;
 					}
@@ -255,8 +255,9 @@ client.on('message', async msg => {
 			break;
             // !second~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 			case 'second':
+			case 'third':
 				let accusedS = guild.members.find(user => user.id === msg.mentions.users.first().id);
-				if (isDay && nominate.accused[accusedS.displayName.toLowerCase()] != null && user.roles.has(discordRoles.Town.id) && accusedS.id != user.id) {
+				if (isDay && vote == null && nominate.accused[accusedS.displayName.toLowerCase()] != null && user.roles.has(discordRoles.Town.id) && accusedS.id != user.id) {
 					let alreadySecond = false;
 					nominate.accused[accusedS.displayName.toLowerCase()].nominations.forEach(nomination => {
 						if (nomination == user.displayName) { alreadySecond = true; }
