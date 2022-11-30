6a132e7cc7e840a35440fe6f6c59b4a6acd4725f
Evening Starlight <EveningStarlight@cmail.carleton.ca>
2022-11-30 13:09:29 -0500

    add basic lobby

---
+++ commands/start.js
@@ -0,0 +1,48 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder } = require("discord.js");
+const { getGame } = require("../scripts/game.js")
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("start")
+    .setDescription("Starts a new game of Werewolf"),
+
+  async execute(interaction) {
+    components = getComponents()
+
+    game = getGame()
+    embeds = await game.lobby.embed()
+
+    const message = {
+        "content": "Let's have a game of werewolf!",
+        "components": components,
+        "embeds": embeds
+    }
+    await interaction.reply(message);
+
+    game.lobby.interaction = interaction
+  },
+};
+
+function getComponents() {
+    const row1 = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('joinLobby')
+					.setLabel('Join Lobby')
+					.setStyle(ButtonStyle.Success))
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('leaveLobby')
+					.setLabel('Leave Lobby')
+					.setStyle(ButtonStyle.Danger),
+			);
+    const row2 = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('startGame')
+					.setLabel('Start Game')
+					.setStyle(ButtonStyle.Primary)
+            );
+
+    return [row1, row2]
+}
