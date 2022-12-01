59a24a7ed44354e7377ad3b0b33c752d52da859c
Evening Starlight <55841241+EveningStarlight@users.noreply.github.com>
2022-12-01 09:09:06 -0500

    add ephemeral role and helpers (#26)

---
+++ commands/helpers/role.js
@@ -0,0 +1,26 @@
+const rolesFile = require('../../scripts/roles.js');
+
+module.exports = {
+    autocomplete: async function autocomplete(interaction) {
+      const focusedValue = interaction.options.getFocused();
+      filtered = rolesFile.getFilteredRoleNames(focusedValue).splice(0,25)
+
+      await interaction.respond(
+          filtered.map(choice => ({ name: choice, value: choice })),
+      );
+  },
+
+    execute: async function execute(interaction, ephemeral=false) {
+      const roleName = interaction.options.getString('name') ?? "error";
+
+      role = rolesFile.roleCheck(roleName);
+
+      if (role != null) {
+          const embed = await rolesFile.getRoleEmbed(role);
+          await interaction.reply({ ephemeral: ephemeral, embeds: [ embed ] });
+
+      } else {
+        await interaction.reply({content:`The role: ${roleName} was not found`, ephemeral: true});
+      }
+    }
+}
