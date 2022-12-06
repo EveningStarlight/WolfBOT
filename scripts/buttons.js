8bff56e078ad44174080158bc2cc3a9639655908
Evening Starlight <EveningStarlight@cmail.carleton.ca>
2022-12-06 16:26:33 -0500

    Add framework for next state
    Adds start game
    adds quit game
    creates channels on start
    deletes channels on quit

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,4 +1,4 @@
-const { getGame } = require('./game.js')
+const { Game } = require('./game.js')
 
 function processButton(interaction) {
     if (interaction.customId == 'joinLobby') {
@@ -6,13 +6,21 @@ function processButton(interaction) {
     } else if (interaction.customId == 'leaveLobby') {
         leaveLobby(interaction)
     } else if (interaction.customId == 'startGame') {
+        startGame(interaction)
+    } else if (interaction.customId == 'quitGame') {
+        quitGame(interaction)
     }
 }
 
 async function joinLobby(interaction) {
-    const game = getGame()
+    const game = Game.get(interaction.member.guild)
 
-    if (!game.lobby.players.includes(interaction.user)) {
+    if (game.state !== 'lobby') {
+        interaction.reply({
+            content: 'The game has already begun!',
+            ephemeral: true,
+        })
+    } else if (!game.lobby.players.includes(interaction.user)) {
         game.lobby.players.push(interaction.user)
         const embed = await game.lobby.embed()
 
@@ -26,9 +34,14 @@ async function joinLobby(interaction) {
 }
 
 async function leaveLobby(interaction) {
-    const game = getGame()
+    const game = Game.get(interaction.member.guild)
 
-    if (game.lobby.players.includes(interaction.user)) {
+    if (game.state !== 'lobby') {
+        interaction.reply({
+            content: 'The game has already begun!',
+            ephemeral: true,
+        })
+    } else if (game.lobby.players.includes(interaction.user)) {
         game.lobby.players = game.lobby.players.filter(function (user) {
             return user != interaction.user
         })
@@ -41,6 +54,35 @@ async function leaveLobby(interaction) {
             ephemeral: true,
         })
     }
+}
+
+async function startGame(interaction) {
+    const game = Game.get(interaction.member.guild)
+
+    if (game.state !== 'lobby') {
+        interaction.reply({
+            content: 'The game has already begun!',
+            ephemeral: true,
+        })
+    } else {
+        game.advanceState()
+        const row2 = interaction.message.components[1]
+        row2.components.shift()
+        row2.components[0].data.disabled = false
+        interaction.update({
+            content: 'The game has now begun!',
+            components: [row2],
+        })
+    }
+}
+
+async function quitGame(interaction) {
+    Game.quit(interaction.member.guild)
+
+    interaction.update({
+        content: 'The game has now finished, thanks for playing everyone!',
+        components: [],
+    })
 }
 
 module.exports = {
